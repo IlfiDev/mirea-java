b169ce05ff2a2c048ca480a15c25a8c2068047e1
IlfiDev <90088441+IlfiDev@users.noreply.github.com>
2021-09-25 11:11:36 +0300

    Add files via upload

---
+++ Practice4/Practice4/src/ru/mirea/practice/Main.java
@@ -0,0 +1,16 @@
+package ru.mirea.practice;
+
+import ru.mirea.practice.Classes.FootballScore;
+import ru.mirea.practice.Classes.LabExample;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	    new FootballScore();
+    }
+}
