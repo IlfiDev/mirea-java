e4f085b0b8f2c4421feafd28a63599b3b517485f
IlfiDev <90088441+IlfiDev@users.noreply.github.com>
2021-09-27 21:11:09 +0300

    Add files via upload

---
+++ Practice8/src/ru/mirea/practice/Main.java
@@ -0,0 +1,37 @@
+package ru.mirea.practice;
+
+import java.io.*;
+
+public class Main {
+    //the same as Third
+    static void First(String str) throws IOException {
+        FileWriter fw = new FileWriter("Text1.txt", false);
+        fw.write(str);
+        fw.flush();
+    }
+    static void Second() throws IOException {
+        FileReader fr = new FileReader("Text1.txt");
+        int c;
+        while((c = fr.read()) != -1){
+            System.out.print((char) c);
+        }
+
+    }
+    static void Fourth(String str) throws IOException {
+        FileWriter fw = new FileWriter("Text1.txt", true);
+        fw.write(str);
+        fw.close();
+    }
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String str = br.readLine();
+
+        First(str);
+        Second();
+        str = br.readLine();
+        Fourth(str);
+        Second();
+    }
+}
