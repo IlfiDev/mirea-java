5484266630a53050e4b3a5075ab7af8185969daf
Ilya <90088441+IlfiDev@users.noreply.github.com>
2021-10-30 09:18:34 +0300

    Add files via upload

---
+++ Practice6/src/com/ilfidev/Main.java
@@ -0,0 +1,18 @@
+package com.ilfidev;
+
+import com.ilfidev.classes.Execution;
+import com.ilfidev.classes.Student;
+import com.ilfidev.classes.StudentInserter;
+import com.ilfidev.classes.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Test test = new Test();
+        test.start();
+    }
+}
