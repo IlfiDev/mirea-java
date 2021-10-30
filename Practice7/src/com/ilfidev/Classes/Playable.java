da3a061bbbebb77245f078a81f7937d48bd60fcc
Ilya <90088441+IlfiDev@users.noreply.github.com>
2021-10-30 10:58:44 +0300

    Add files via upload

---
+++ Practice7/src/com/ilfidev/Classes/Playable.java
@@ -0,0 +1,9 @@
+package com.ilfidev.Classes;
+
+public interface Playable {
+    void convertStringToDeck();
+    int getDeckSize();
+    void addCard(int item);
+    int showCard();
+    int LoseCard();
+}
---
+++ Practice7/src/com/ilfidev/Main.java
@@ -0,0 +1,13 @@
+package com.ilfidev;
+
+import com.ilfidev.Classes.*;
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	Settings setting = new Settings();
+    setting.start();
+    }
+}
