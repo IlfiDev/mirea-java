0ab49a027dea11d25075906c517e8b888f84b215
Ilya Filippov <90088441+IlfiDev@users.noreply.github.com>
2021-11-04 16:01:25 +0300

    Add files via upload

---
+++ Practice11/src/com/ilfidev/Main.java
@@ -0,0 +1,17 @@
+package com.ilfidev;
+
+import com.ilfidev.Classes.PhoneNumber;
+import com.ilfidev.Classes.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        Test test = new Test();
+        test.start();
+    }
+}
